12989246ade6f460fe8b093799e4527341a64724
Hemeswar <hemeswar.polina@gmail.com>
2020-09-30 20:17:38 +0530

    stuffguycommit

--- src/Room.java
+++ src/Room.java
@@ -5,6 +5,15 @@ public class Room implements Decorable{
  
 	public static void main(String[] args) {
 		System.out.println("Decorating room with Color: "+Room.color);
+		
+		System.out.println("Added post gitcloned");
+		
+		System.out.println("check the colloberation");
+		
+		//step1 is again staging 
+		
+		//step 2 is commit 
+		System.out.println("here it goes");
 	}
  
 }
