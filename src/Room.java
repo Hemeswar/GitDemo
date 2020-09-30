7c799d2c1e6a598c57ee9b3b4f58488e71fa4a9d
Hemeswar <hemeswar.polina@gmail.com>
2020-09-30 19:41:24 +0530

    FirstCommit

---
+++ src/Room.java
@@ -0,0 +1,10 @@
+interface Decorable {
+	 String color="Blue";
+}
+public class Room implements Decorable{
+ 
+	public static void main(String[] args) {
+		System.out.println("Decorating room with Color: "+Room.color);
+	}
+ 
+}
